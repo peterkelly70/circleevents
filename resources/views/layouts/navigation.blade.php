ecf97f334158b5a1fd22a3626168b9688f00a88c
Peter Kelly <peter@computer-wiazard.com.au>
2026-04-09 00:33:17 +0800

    Show logout alongside main account actions

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -22,37 +22,21 @@
                 </div>
             </div>
 
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <button class="inline-flex items-center rounded-full border border-white/10 bg-white/5 px-3 py-2 text-sm font-medium leading-4 text-stone-300 transition ease-in-out duration-150 hover:bg-white/10 hover:text-white focus:outline-none">
-                            <div>{{ Auth::user()->name }}</div>
+            <div class="hidden sm:flex sm:items-center sm:gap-3 sm:ms-6">
+                <span class="rounded-full border border-white/10 bg-white/5 px-4 py-2 text-sm font-medium text-stone-300">
+                    {{ Auth::user()->name }}
+                </span>
 
-                            <div class="ms-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                        </button>
-                    </x-slot>
+                <x-nav-link :href="route('profile.edit')" :active="request()->routeIs('profile.*')">
+                    {{ __('Profile') }}
+                </x-nav-link>
 
-                    <x-slot name="content">
-                        <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
-                        </x-dropdown-link>
-
-                        <!-- Authentication -->
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </x-dropdown-link>
-                        </form>
-                    </x-slot>
-                </x-dropdown>
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium leading-5 text-stone-400 transition duration-150 ease-in-out hover:border-stone-300 hover:text-stone-200 focus:outline-none focus:text-stone-200 focus:border-stone-300">
+                        {{ __('Log Out') }}
+                    </button>
+                </form>
             </div>
 
             <div class="-me-2 flex items-center sm:hidden">
